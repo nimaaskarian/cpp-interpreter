8502911110726bb09c69c8ef02ad7d5005d4f9e0
nimaaskarian <nimaas1400@gmail.com>
2022-10-16 10:44:29 +0330

    Added multiple file support

--- cpp.sh
+++ cpp.sh
@@ -4,32 +4,42 @@ if [ $# -eq 0 ]; then
   echo $noFileEr
   exit 1
 fi
-input=$1
-if [[ "$1" == "-pwd" ]]; then
-  dir=$PWD
-  input=$2
-elif [[ "$2" == "-pwd" ]]; then
-  dir=$PWD
+
+for arg in "$@"
+do 
+  if [[ "$arg" == "-pwd" ]]; then
+    dir=$PWD
+  fi
+done
+
+if [[ "$dir" == "" ]]; then 
+  if [[ $# -eq 0 ]]; then 
+    echo $noFileEr
+    exit 1
+  fi
+elif [[ $# -lt 2 ]]; then 
+    echo $noFileEr
+    exit 1
 fi
 
-if [ $input == "" ]; then 
-  echo $noFileEr
-  exit 1
-fi
-
-if [ -f $input ]; then 
-  if [[ $dir == "" ]]; then
-    dir=/tmp/cpp-interpreter
+for input in "$@"
+do
+  if [[ ! "$input" == "-pwd" ]]; then 
+    if [ -f "$input" ]; then 
+      if [[ $dir == "" ]]; then
+        dir=/tmp/cpp-interpreter
+      fi
+      output=$dir/${input%.*}
+      if [ ! -d $dir ]; then
+        mkdir $dir
+      fi
+      g++ "$input" -o "$output"
+      if [ $? -eq 0 ]; then 
+        "$output"
+      fi
+    else 
+      echo "File $input not found !!"
+      exit 1
+    fi
   fi
-  output=$dir/${input%.*}
-  if [ ! -d $dir ]; then
-    mkdir $dir
-  fi
-  g++ "$input" -o "$output"
-  if [ $? -eq 0 ]; then 
-    "$output"
-  fi
-else 
-  echo "File not found !!"
-  exit 1
-fi
+done
