b6ab11e894a2f80f7fed3ab974b4813871b7dad5
nimaaskarian <nimaas1400@gmail.com>
2023-06-15 17:06:26 +0330

    Added arg parse, compiler selection, run options selecting directory

--- cpp.py
+++ cpp.py
@@ -1,20 +1,33 @@
 #!/bin/python3
-import sys
-import subprocess
-import os
-import time
-import hashlib
+import sys, subprocess, os, time, hashlib, argparse
 
 default_dir = "/tmp/cpp-interpreter/"
-defined_args=["-sd", "-wd","--pkg", "-m","-M", "-q", "-j","-h","-r","--help","--stdin","--gcc","--repeat","--force"]
+# defined_args=["-sd", "-wd","--pkg", "-m","-M", "-q", "-j","-h","-r","--help","--stdin","--gcc","--repeat","--force","-o"]
 
-args = sys.argv[1:]
-if "--pkg" in args:
-    pkgIndex=args.index("--pkg")+1
-    pkgs=args[pkgIndex]
-    args=args[0:pkgIndex]+args[pkgIndex+1:]
+# args = sys.argv[1:]
+parser = argparse.ArgumentParser(prog=sys.argv[0])
+parser.add_argument('-q', '--quite',dest="quite", action="store_true")
+parser.add_argument('-r', '--repeat',dest="repeat", action="store_true")
+parser.add_argument('-j', '--join',dest="join", action="store_true")
+parser.add_argument('-c', '--compiler=',dest="compiler", action="store",help="backend compiler (default is g++)",default="g++")
+parser.add_argument('-m', '--minimal-paths', dest="minimal", action="store_true",help="Minimal file paths")
+parser.add_argument('-M', '--no-compile-messages', dest="no_compile_messages",
+                    action="store_true",help="Don't print compile messages")
+parser.add_argument('-f', '--force', dest="force", action="store_true",help="Force recompile")
+parser.add_argument('-o', '--output=', dest="output", action="store",help="Output name")
+parser.add_argument('-Oc', '--compiler-options=', dest="compiler_options", action="store",help="Compiler options",default=[])
+parser.add_argument('-Or', '--run-options=', dest="run_options", action="store",help="Run options",default=[])
+parser.add_argument('-sd', '--select-directory=', dest="select_directory", action="store",help="Select a directory to compile to",default="")
+parser.add_argument('-wd', '--working-directory', dest="working_directory", action="store_true",help="Compile in working directory")
+parser.add_argument('-', '--stdin', dest="stdin", action="store_true",help="Get input from stdin")
+parser.add_argument('--pkg=', dest="pkg", action="store",help="Append pkg-config flag and libs to compiler")
+parser.add_argument('files', nargs='*', action="store")
+args = parser.parse_args()
+
+if len( sys.argv ) <= 1 or not len(args.files):
+    parser.print_usage()
 def init():
-    if (hasArgs("--stdin")):
+    if (args.stdin):
         filename = "/tmp/cpy-stdin-"+str(time.time())+".cpp"
         f = open(filename, "a")
         try:
@@ -24,51 +37,23 @@ def init():
         except KeyboardInterrupt:
             conPrint("\n"+info_msg.format('recieving inputs ended'))
         f.close()
-        args.append(filename)
-    if hasArgs(["-h", "--help"]) or len(args) == 0:
-        conPrint("""Usage: cpy [FILES...] [OPTIONS...] [G++ OPTIONS...]
+        args.files.append(filename)
 
-Help Option:
-  -h, --help         Shows help options
-
-Application Options:
-  -m                 Minimal file paths
-  -M                 Don't show compile info
-  -j                 Compile FILES into one binary
-  -q                 Quite (no messages)
-  -wd                Export binary in working directory
-  -sd                Export binary in same directory as .cpp file
-  --force            Force recompile
-  --pkg              Append pkg-config flag and libs to compiler
-  --stdin            Gets input from stdin
-  --gcc              Use gcc instead of g++ (for c language)
-  -r,--repeat        Repeats compiling and running. hit ^C repeatedly to abort
-
-G++ Options:
-  Any options beside Application Options will be passed 
-  to G++ itself when using '-j'. If you're getting compile
-  errors and you don't know why, its probably an issue with an option.""")
-        return
-
-    if hasArgs("-j"):
-        files = [arg for arg in args if arg not in defined_args]
-        dir = make_dir_name(files[0])
-        print_name = make_print_name(files)
-        return run(compile(files,os.path.join(dir, make_compound_hash(files)),print_name),print_name)
+    if args.join:
+        dir = make_dir_name()
+        print_name = make_print_name(args.files)
+        return run(compile(args.files,os.path.join(dir, make_compound_hash(args.files)),print_name,dir),print_name)
     else:
-        files=[arg for arg in args if arg not in defined_args and not arg.startswith("-")]
-        compiler_args=[arg for arg in args if arg not in defined_args and arg.startswith("-")]
-        for file in files:
-            if file in defined_args: continue
+        for file in args.files:
             print_name = make_print_name([file])
 
             if not os.path.exists(file):
                 conPrint(filenotfound_error_msg.format(print_name))
                 continue
-            dir = make_dir_name(file)
-            return run(compile([file],make_compile_path(dir, file),print_name,compiler_args),print_name)
+            dir = make_dir_name()
+            return run(compile([file],make_compile_path(dir, file),print_name,dir),print_name)
 
-    if (hasArgs("--stdin")):
+    if (args.stdin):
         os.remove(filename)
 
 class bcolors:
@@ -94,9 +79,9 @@ compile_error_msg = error_msg.format("compile failed")
 running_error_msg = error_msg.format("something went wrong in running")
 interrupt_error_msg = error_msg.format("user keyboard interrupt (exited)")
 
-def make_dir_name(path):
-    if hasArgs("-sd"): return os.path.dirname(path)
-    if hasArgs("-wd"): return os.path.dirname("./")
+def make_dir_name():
+    if args.select_directory: return os.path.dirname(args.select_directory)
+    if args.working_directory: return os.path.dirname("./")
     if not os.path.exists(default_dir):
         os.makedirs(default_dir)
     return default_dir
@@ -114,14 +99,19 @@ def file_hash(path):
     return hashlib.md5( f.read().encode() ).hexdigest()
 
 def make_print_name(paths):
-    if hasArgs("-m"): 
+    if args.minimal: 
         return ", ".join([os.path.basename(path) for path in paths])
     return ", ".join(paths)
 
 def make_compile_path(dir,path):
     # filename=os.path.basename(path)
     # filename_without_ext = os.path.splitext(filename)[0]
-    return os.path.join(dir, file_hash(path))
+    if args.output:
+        name = args.output
+    else:
+        name = file_hash(path)
+
+    return os.path.join(dir, name)
     
 
 def run(file,fullname):
@@ -129,61 +119,49 @@ def run(file,fullname):
     conPrint(running_msg.format(fullname)+bcolors.ENDC)
     code=1
     try:
-        subprocess.run([file])
+        subprocess.run([os.path.join(args.select_directory,file)]+args.run_options)
         code=0
     except KeyboardInterrupt: 
         conPrint("\n"+interrupt_error_msg)
         code=1
-    except Exception:
+    except Exception as e:
+        print(e)
         conPrint("\n"+running_error_msg)
     finally: return code
 
-def compile(inputs,output,filename,compile_args=[]):
+def compile(inputs,output,filename,dir,compile_args=args.compiler_options):
 
-    if not hasArgs("--force"):
-        if hasArgs("-j"):
-            if (os.path.isfile(default_dir+make_compound_hash(inputs))):
+    if not args.force:
+        if args.join:
+            if (os.path.isfile(os.path.join(dir, make_compound_hash(inputs)))):
                 return output
         else:
-            inputs = [ input for input in inputs if not os.path.isfile(default_dir+file_hash(input))]
+            inputs = [ input for input in inputs if not os.path.isfile(make_compile_path(dir,input))]
 
     if not len(inputs):
         return output
     start_time = time.time()
     conPrint(compiling_msg.format(filename)+bcolors.ENDC)
-    compiler = "gcc" if hasArgs("--gcc") else "g++"
-    if hasArgs("--pkg"):
-        compile_args += subprocess.check_output("pkg-config --cflags "+pkgs, shell=True).split()
-        compile_args += subprocess.check_output("pkg-config --libs "+pkgs, shell=True).split()
-    child = subprocess.Popen([compiler] +compile_args+ inputs +   ["-o", output ],stdout=subprocess.PIPE)
+    if args.pkg:
+        compile_args += subprocess.check_output("pkg-config --cflags "+args.pkg, shell=True).split()
+        compile_args += subprocess.check_output("pkg-config --libs "+args.pkg, shell=True).split()
+    child = subprocess.Popen([args.compiler] +compile_args+ inputs +   ["-o", output ],stdout=subprocess.PIPE)
     child.communicate()
     if child.returncode != 0:
         conPrint(compile_error_msg)
         return ""
 
-    if not hasArgs("-M"):
+    if not args.no_compile_messages:
         conPrint(compiled_msg.format(str(round((time.time()-start_time)*1000)/1000) + "s")+bcolors.ENDC)
     return output
 
-def hasArgs(input):
-    # check if input is a list
-    doesntExist = Exception(error_msg.format(f"Argument {input} doesn't exist"))
-    if type(input) != list: 
-        if not input in defined_args: raise doesntExist
-        return input in args
-    # input its a list here
-    for item in input:
-        if not item in defined_args: raise doesntExist
-        if item in args: return True
-    return False
-
 def conPrint(*messages):
-    if not hasArgs("-q"):
+    if not args.quite:
         print(*messages)
 
 
 while True:
     if init()==1:
         break
-    if not hasArgs(["-r","--repeat"]):
+    if not args.repeat:
         break
